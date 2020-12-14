0db15e8aa9c7e2911ca10634cfe095433d8c26c6
victormanuel1998 <72715880+victormanuel1998@users.noreply.github.com>
2020-12-14 15:10:36 -0600

    sql

--- app/src/main/java/com/example/sql/ModelRecord.java
+++ app/src/main/java/com/example/sql/ModelRecord.java
@@ -3,9 +3,7 @@ package com.example.sql;
 public class ModelRecord {
 
     String id, product_name, image, brand, model, serialnumber, price, description,TELEFONO;
-
-
-    public ModelRecord(String s, String s1, String s2, String s3, String s4, String s5, String s6, String s7) {
+    {
 
     }
 
